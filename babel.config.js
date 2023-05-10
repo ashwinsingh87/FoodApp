c7c0fe7f3b637bee7829ac2224ce89fd7562b771
Ashwin Singh <91330777+ashwinsingh87@users.noreply.github.com>
2023-05-10 15:56:19 +0530

    Netlify host

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
-module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      { "targets": { "node": "current" } },
-    ],
-    ["@babel/preset-react",{"runtime":"automatic"}],
-  ],
-};
+// module.exports = {
+//   presets: [
+//     [
+//       "@babel/preset-env",
+//       { "targets": { "node": "current" } },
+//     ],
+//     ["@babel/preset-react",{"runtime":"automatic"}],
+//   ],
+// };
